a35b3d79cdda19428024cd896d02218d39860618
iravillanuevaa <ira_villanueva@dlsu.edu.ph>
2020-07-29 21:21:51 +0800

    added index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+// import modules express and handlebars
+const express = require('express');
+const hbs = require('hbs');
+
+// define css, img, js, and views as static 
+app.use(express.static('public'));
+app.use(express.static('views'));
+
+// parses incoming requests with urlencoded payloads
+app.use(express.urlencoded({extended: true}));
+
+// import routes module
+const routes = require('./routes/routes.js');
+
+//partials
+hbs.registerPartials(__dirname + '/views/partials');
+
+// import '' module
+const db = require('./models/db.js');
+
+// define the paths contained in routes module
+app.use('/', routes);
+
+// set hbs as view engine
+app.set('view engine', 'hbs');
+
+// connects to the database
+db.connect();
+
+// binds the server to a specific port
+const port = 9090;
+app.listen(port, function () {
+    console.log('app listening at port ' + port);
+});
