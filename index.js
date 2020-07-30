9eacfd092153e33b4f0b4e7e765ac938191f9ba0
Jacob Darvin <jacobisdarvin@gmail.com>
2020-07-30 09:55:34 +0800

    installed express-hbs, path
    created folders for hbs, turned index.html to hbs etc.

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-// import modules express, db, and handlebars
+// IMPORT
 const express = require('express');
 const hbs = require('hbs');
+const exphbs = require('express-handlebars');
+var path = require('path');
+
+//DATA BASE AND EXPRESS
 const db = require('./models/db.js');
 const app = express();
 
-// define css, img, js, and views as static 
+// DEFINE STATIC FOLDERS
 app.use(express.static('public'));
 app.use(express.static('views'));
 
 // set hbs as view engine
 app.set('view engine', 'hbs');
+
+app.engine('hbs', exphbs({
+	extname: 'hbs',
+	defaultView: 'main',
+	layoutsDir: path.join(__dirname, '/views/layouts'),
+	partialsDir: path.join(__dirname, '/views/partials'),
+}));
 
 // parses incoming requests with urlencoded payloads
 app.use(express.urlencoded({extended: true}));
@@ -18,7 +29,8 @@ app.use(express.urlencoded({extended: true}));
 const routes = require('./routes/routes.js');
 
 //partials
-//hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(__dirname + '/views/partials');
+//
 
 // set the folder `public` as folder containing static assets (css, js, imgs)
 app.use(express.static('public'));
--- routes/routes.js
+++ routes/routes.js
@@ -9,5 +9,13 @@ const app = express();
 // call function getIndex when client sends a request for '/' defined in routes.js
 app.get('/', controller.getIndex);
 
+
+//Home Route
+app.get('/home(page)?(.html)?', function(req, res) {
+    res.render('index', {
+        title: 'Home | BookMeDental',
+    })
+});
+
 // enables to export app object when called in another .js file
 module.exports = app;
