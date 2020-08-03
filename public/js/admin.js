be4c890fbc428d4d923044b15e050750e6d09cf2
Jacob Darvin <jacobisdarvin@gmail.com>
2020-08-03 22:27:06 +0800

    added admin, changed html, anchored navbar

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,26 +1,28 @@
 //define some sample data
  var tabledata = [
-    {id:1,fname:"Jacob", lname:"Darvin", name:"Valorant", email:"jacobisdarvin@gmail.com", contact:"0918894", dob:"1/20/20"},
-    {id:2,fname:"Jacob", lname:"Darvin", name:"White Teeth Inc", email:"jacobisdarvin@gmail.com", contact:"2381203", dob:"1/20/20"},
-    {id:3,fname:"Jacob", lname:"Darvin", name:"Clinic Passion", email:"jacobisdarvin@gmail.com", contact:"1324124", dob:"1/20/20"},
-    {id:4,fname:"Jacob", lname:"Darvin", name:"Arthaland Dental", email:"jacobisdarvin@gmail.com", contact:"2133321", dob:"1/20/20"},
-    {id:5,fname:"Jacob", lname:"Darvin", name:"Counter Strike", email:"jacobisdarvin@gmail.com", contact:"2141242", dob:"1/20/20"},
-    {id:6,fname:"Jacob", lname:"Darvin", name:"Dota", email:"jacobisdarvin@gmail.com", contact:"1983921", dob:"1/20/20"},
+    {id:1,fname:"Jacob", lname:"Darvin", name:"Valorant", email:"jacobisdarvin@gmail.com", contact:"0918894", dob:"1/20/20", car:"false"},
+    {id:2,fname:"Jacob", lname:"Darvin", name:"White Teeth Inc", email:"jacobisdarvin@gmail.com", contact:"2381203", dob:"1/20/20", car:"true"},
+    {id:3,fname:"Jacob", lname:"Darvin", name:"Clinic Passion", email:"jacobisdarvin@gmail.com", contact:"1324124", dob:"1/20/20", car:"false"},
+    {id:4,fname:"Jacob", lname:"Darvin", name:"Arthaland Dental", email:"jacobisdarvin@gmail.com", contact:"2133321", dob:"1/20/20", car:"false"},
+    {id:5,fname:"Jacob", lname:"Darvin", name:"Counter Strike", email:"jacobisdarvin@gmail.com", contact:"2141242", dob:"1/20/20", car:"true"},
+    {id:6,fname:"Jacob", lname:"Darvin", name:"Dota", email:"jacobisdarvin@gmail.com", contact:"1983921", dob:"1/20/20", car:"true"},
  ];
 
 var table = new Tabulator("#example-table", {
     resizableRows:false,
     resizableColumns:true,
-    height:205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+    pagination:"local",
+    paginationSize:6,
     data:tabledata, //assign data to table
     layout:"fitColumns", //fit columns to width of table (optional)
     columns:[ //Define Table Columns
-        {title:"First Name", field:"fname", width:150},
-        {title:"Last Name", field:"lname", width:150},
-        {title:"Clinic Name", field:"name", width:150},
+        {title:"First Name", field:"fname"},
+        {title:"Last Name", field:"lname"},
+        {title:"Clinic Name", field:"name"},
         {title:"Employer Email", field:"email"},
         {title:"Contact", field:"contact"},
         {title:"Date Filed", field:"dob", sorter:"date", hozAlign:"center"},
+        {title:"Status", field:"car", hozAlign:"center", formatter:"tickCross"},
     ],
     rowClick:function(e, row){ //trigger an alert message when the row is clicked
         alert("Entry " + row.getData().id + " clicked");
