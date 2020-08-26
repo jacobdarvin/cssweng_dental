1d4d0d18d258d307b5dbe82c19283053146e1e53
iravillanuevaa <ira_villanueva@dlsu.edu.ph>
2020-08-27 00:50:32 +0800

    updated feed controller

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -4,28 +4,6 @@ const Employer = require('../models/EmployerModel');
 
 const feedController = {
     getFeed: function (req, res) {
-    //    db.findMany(Job, { employer: req.session.user }, '', function(result){
-
-    //     console.log(result)
-    //         // if(result){
-    //         //     result
-    //         //         // .populate({
-    //         //         //     path: 'employer',
-    //         //         //     model: 'Job'
-    //         //         // })
-    //         //         .populate('employer')
-    //         //         .lean()
-    //         //         .execPopulate(function(err, data){
-    //         //             res.render('feed', {
-    //         //                 active_session: (req.session.user && req.cookies.user_sid),
-    //         //                 active_user: req.session.user,
-    //         //                 title: 'Job Feed | BookMeDental',
-    //         //                 profile_active: true,
-    //         //                 jobs: result,
-    //         //             });
-    //         //         })
-    //         // }
-    //    })
         db.findOne(Employer, {account: req.session.user}, '_id', function(emp){
             db.findMany(Job, {employer: emp._id}, '', function(result){
                 Employer.populate(result, {path: 'employer', options: {lean: true}}, function (err, data){
@@ -38,21 +16,6 @@ const feedController = {
                         jobs: data
                     });
                 })
-                // if(result)
-                //     result
-                //         .populate({
-                //             path: 'employer'
-                //         })
-                //         .execPopulate(function(err,data){
-                //             if (err) throw err;
-                //                 res.render('feed', {
-                //                 active_session: (req.session.user && req.cookies.user_sid),
-                //                 active_user: req.session.user,
-                //                 title: 'Job Feed | BookMeDental',
-                //                 profile_active: true,
-                //                 jobs: data,
-                //             });
-                //         })
             })
         })
 
